110f0de046703e3b22a9a52d007259761d376a88
ricarthlima <rrsl@cin.ufpe.br>
2024-11-19 12:26:26 -0300

    v2.4 - localizando imagens

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -40,7 +40,7 @@ class _SplashScreenState extends State<SplashScreen> {
               child: Padding(
                 padding: const EdgeInsets.only(top: 16),
                 child: Image.asset(
-                  'assets/images/grimorio_titulo_1152.png',
+                  _getImagePathByLanguage(),
                   width: 300,
                 ),
               ),
@@ -75,5 +75,21 @@ class _SplashScreenState extends State<SplashScreen> {
         beginPosition: const Offset(1, 0),
       ),
     );
+  }
+
+  String _getImagePathByLanguage() {
+    String code = Localizations.localeOf(context).languageCode;
+    print(code);
+
+    switch (code) {
+      case "pt":
+        return 'assets/images/grimorio_titulo_1152.png';
+      case "en":
+        return 'assets/images/grimorio_titulo_1152-en.png';
+      case "es":
+        return 'assets/images/grimorio_titulo_1152-es.png';
+    }
+
+    return 'assets/images/grimorio_titulo_1152-en.png';
   }
 }
