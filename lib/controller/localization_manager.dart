09a97722d0da5f8838015d7830ba05c44c056718
Ricarth Lima <ricarth.lima@gmail.com>
2024-11-19 16:41:18 -0300

    v4.2 - configurando o servico

--- lib/controller/localization_manager.dart
+++ lib/controller/localization_manager.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LocalizationManager with ChangeNotifier {
   String languageCode;
@@ -27,11 +30,25 @@ class LocalizationManager with ChangeNotifier {
     },
   };
 
-  setLanguage(String newCode) {
+  Future<void> setLanguage(String newCode) async {
+    await _getLanguageFromServer(newCode);
     languageCode = newCode;
     notifyListeners();
   }
 
+  Future<void> _getLanguageFromServer(String newCode) async {
+    String url =
+        "https://gist.githubusercontent.com/ricarthlima/52c31eacaf0f28ba7a49e45e0adca89d/raw/cb6a4773731423e6cff91b6a7cfc8198be2ca04d/app_$newCode.json";
+
+    http.Response httpResponse = await http.get(Uri.parse(url));
+    Map<String, dynamic> response = json.decode(httpResponse.body);
+
+    _mapLanguages[newCode] = response.map(
+      (key, value) => MapEntry(key, value.toString()),
+    );
+  }
+
+  // Sentenças
   String _getSentence(String keySentence) {
     String? sentence = _mapLanguages[languageCode]?[keySentence];
     sentence ??= _mapLanguages[defaultLanguageCode]![keySentence]!;
